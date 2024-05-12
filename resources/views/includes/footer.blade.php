123b88c31e4f4d250860dcd1aeb7ad17d59dbf6f
Pablo Henrique <pabllobeg@gmail.com>
2024-05-12 13:29:39 -0300

    chore: remove footer logo

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,15 +2,6 @@
     <div class="row">
         <div class="col-lg-6 col-sm-12">
             <div class="footer-info">
-                <div>
-                    <img src="{{ asset('assets/images/logo.png') }}" alt="" class="footer-logo" width="">
-                    <img src="{{ asset('/assets/images/+18.png') }}" alt="" width="38">
-                </div>
-                <p class="{{ url('/sobre-nos') }}">
-                    <strong>{{ getenv('APP_NAME') }}</strong> é uma comunidade dedicada a oferecer a melhor experiência aos jogadores.
-                    Estamos confiantes de que o JOGUINHO revolucionará a indústria de cassinos online.
-                    Experimente! Divirta-se jogando e conquiste vitórias!
-                </p>
             </div>
         </div>
         <div class="col-lg-6 col-sm-12">
