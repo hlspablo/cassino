5180c9198e7c599a016ccfaa48b0ce6ffcdeb846
Pablo Henrique <pabllobeg@gmail.com>
2024-06-01 16:20:48 -0300

    feat: added new icons for categories

--- resources/views/includes/navbar_left.blade.php
+++ resources/views/includes/navbar_left.blade.php
@@ -12,28 +12,16 @@
         <ul class="navbar_list">
             <li class="navbar_list_links">
                 <a href="{{ url('/') }}" title="Como funciona?">
-                    <img src="{{ asset('/assets/images/svg/home2.svg') }}" alt="" width="24">
+                    <i class="fa-solid fa-house fa-xl mr-2"></i>
                     Visão geral
                 </a>
             </li>
             <li class="navbar_list_links">
                 <a href="{{ url('painel/affiliates') }}" title="Menu de Afiliado">
-                    <img src="{{ asset('/assets/images/svg/affiliate.svg') }}" alt="" width="24">
+                    <i class="fa-solid fa-bullhorn fa-xl mr-2"></i>
                     Menu de Afiliado
                 </a>
             </li>
-
-            <div class="mt-5 navbar_menu_title">
-                <h4>JOGOS DA CASA</h4>
-            </div>
-            @foreach(\App\Models\GameExclusive::limit(5)->orderBy('views', 'desc')->where('active', 1)->get() as $gameExclusive)
-                <li class="navbar_list_links">
-                    <a href="{{ route('web.vgames.show', ['game' => $gameExclusive->uuid]) }}">
-                        <img src="{{ asset('storage/'.$gameExclusive->icon) }}" alt="" width="24">
-                        {{ $gameExclusive->name }}
-                    </a>
-                </li>
-            @endforeach
 
             <div class="mt-5 navbar_menu_title">
                 <h4>CATEGORIAS</h4>
@@ -42,7 +30,7 @@
             @foreach(\App\Models\Category::all() as $category)
                 <li class="navbar_list_links">
                     <a href="{{ route('web.category.index', ['slug' => $category->slug]) }}" title="{{ $category->name }}">
-                        <img src="{{ asset('storage/'.$category->image) }}" alt="{{ $category->name }}" width="24">
+                        <i class="{{ $category->image }} fa-xl mr-2"></i>
                         {{ $category->name }}
                     </a>
                 </li>
@@ -54,19 +42,13 @@
 
             <li class="navbar_list_links">
                 <a href="{{ url('/como-funciona') }}" title="Como funciona?">
-                    <img src="{{ asset('assets/images/svg/about.svg') }}" alt="" width="24">
+                    <i class="fa-solid fa-circle-question fa-xl mr-2"></i>
                     Como funciona?
                 </a>
             </li>
-            {{-- <li class="navbar_list_links">
-                <a href="{{ url('/suporte') }}" title="Suporte">
-                    <img src="{{ asset('assets/images/svg/suporte.svg') }}" alt="" width="24">
-                    Suporte
-                </a>
-            </li> --}}
             <li class="navbar_list_links">
                 <a href="{{ url('/sobre-nos') }}" title="Sobre Nós">
-                    <img src="{{ asset('assets/images/svg/sobre.svg') }}" alt="" width="24">
+                    <i class="fa-solid fa-circle-info fa-xl mr-2"></i>
                     Sobre Nós
                 </a>
             </li>
